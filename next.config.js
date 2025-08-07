82ac307e01db343bbd135291a64682cc271aad4d
memoriae <xingyuliu@outlook.sg>
2025-08-07 09:00:53 +0800

    2025-08-07

--- next.config.js
+++ next.config.js
@@ -6,6 +6,24 @@ const nextConfig = {
   productionBrowserSourceMaps: true /** Missing source maps for large first-party JavaScript */,
   images: {
     formats: ["image/avif", "image/webp"],
+    unoptimized: false,
+    path: '/_next/image',
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/_next/image(.*)',
+        headers: [
+          {
+            key: 'Content-Disposition',
+            value: 'inline',
+          },
+        ],
+      },
+    ];
   },
 };
 
